06b1204ebf5ce172d4fc3ded90f5a66369ee9c2f
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2018-09-21 08:59:18 -0400

    Host page
    * create index-orig.html as backup
    * create ring server for index.html

---
+++ src/hello_world/server.clj
@@ -0,0 +1,28 @@
+(ns hello-world.server)
+
+;; define index content
+(def home
+  "<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <link href=\"css/style.css\" rel=\"stylesheet\" type=\"text/css\">
+    <link rel=\"icon\" href=\"https://clojurescript.org/images/cljs-logo-icon-32.png\">
+  </head>
+  <body>
+    <div>App should be here</div>
+    <div id=\"app\"></div>
+    <script src=\"/cljs-out/dev-main.js\" type=\"text/javascript\"></script>
+  </body>
+</html>")
+
+(defn handler [request]
+  (if (and (= :get (:request-method request))
+           (= "/"  (:uri request)))
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body home}
+    {:status 404
+     :headers {"Content-Type" "text/plain"}
+     :body "Not Found"}))
