f70d1e213fb807658284d0f4abea02c77a3a6bba
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2018-09-21 12:57:58 -0400

    Auto testing

--- tests/hello_world/core_tests.cljs
+++ tests/hello_world/core_tests.cljs
@@ -1,10 +1,15 @@
 (ns hello-world.core-tests
   (:require [cljs.test :refer-macros [deftest is run-tests]]))
 
+;; http://localhost:9500/figwheel-extra-main/auto-testing
+
 ;; (deftest should-not-pass
 ;;   (is (= 1 20)))
 
 (deftest should-pass
   (is (= 1 1)))
+
+(deftest should-also-pass
+  (is (= 2 2)))
 
 (run-tests)
